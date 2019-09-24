327eded3941c6405ba23071e9fc11caf3bb7939a
Szymon Grzemski <sz.grzemski@gmail.com>
2019-09-24 17:55:09 +0200

    Moar

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
 from pyhocon import ConfigFactory
+import os
 configuration = ConfigFactory.parse_file('application.conf')
-# datasets = configuration.get('datasets')
+gztan_path = configuration.get('datasets.gztan.path')
+
+def getFileList(path):
+  filelist = []
+  for root, dirs, files in os.walk(path):
+    for file in files:
+      if file.endswith(".wav"):
+        filelist.append(os.path.join(root, file))
+  return filelist
+
+samples = getFileList(gztan_path)
